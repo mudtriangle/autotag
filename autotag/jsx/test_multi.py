1308bb89def2f8994c5370e335d17acc68f53b26
Marcelo Sandoval-Castaneda <jms1595@nyu.edu>
2020-06-09 13:23:13 +0400

    Removed irrelevant files

--- autotag/jsx/test_multi.py
+++
@@ -1,9 +0,0 @@
-from get_transcript import get_transcript_video
-import multiprocessing
-
-vids = ['/Users/msandov/Desktop/Capstone/test_data/taxi_driver_004-0.mov', '/Users/msandov/Desktop/Capstone/test_data/taxi_driver_004-0.mov']
-
-pool = multiprocessing.Pool(multiprocessing.cpu_count())
-for i in pool.imap_unordered(get_transcript_video, vids):
-    print(i)
-pool.close()
